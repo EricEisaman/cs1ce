80e1eefa061c876ee0cb6ea0edd2ba9beeaf6a78
Glitch (team-beast) <none>
2022-01-16 19:29:38 +0000

    basic app state test example

--- public/staging/cs1-app.js
+++ public/staging/cs1-app.js
@@ -1,17 +1,35 @@
 (function () {
-	'use strict';
+  'use strict';
 
-	// App source code.
-	(async ()=>{
+  (async () => {
+    // create blue box
+    window.box = await CS1.scene.add("a-box");
+    console.log("window.box");
+    console.log(window.box);
+    window.box?.setAttribute("color", "blue");
+    window.box?.object3D.position.set(0, 1, -5);
+    
+    //add a-cursor as child of camera
+    const cursor = document.createElement('a-cursor','');
+    CS1.cam.entity.appendChild(cursor);
+    window.box.addEventListener("click", e => {
+      StateManager.dispatch({type:"path-mutation", payload:{
+        path: "house.rooms.bathroom.locked" , value: (window.box.getAttribute("color")==="green")? false:true
+      }});
+    });
+    
+    StateManager.subscribe("house.rooms.bathroom.locked", ()=>{
+      window.box.setAttribute("color", (window.box.getAttribute("color")==="green")? "red":"green");
+    });
+    
+  })();
 
-	console.log("STARTING APP ...");
-	window.box = await CS1.scene.add("a-box");
-	console.log("window.box");
-	console.log(window.box);
-	window.box?.setAttribute("color","blue");
-	window.box?.object3D.position.set(0,1,-5);
-	 
-	})();
+  // App source code.
+  (async ()=>{
+
+  console.log("STARTING APP ...");
+   
+  })();
 
 })();
 //# sourceMappingURL=cs1-app.js.map
